f7243767ee7e8c8829dca87f0bcbd1b787c85925
Zubair Shaikh <zubairify@gmail.com>
2021-04-05 12:03:43 +0530

    Maven Maths

---
+++ mvn-math/src/test/java/com/ibm/math/TestMaths.java
@@ -0,0 +1,37 @@
+package com.ibm.math;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestMaths {
+	
+	private static Maths maths;
+	
+	@BeforeAll
+	public static void setup() {
+		maths = new Maths();
+	}
+	
+	@Test
+	public void testAdd() {
+		assertEquals(10, maths.add(5, 5));
+	}
+	
+	@Test
+	public void testSubs() {
+		assertEquals(10, maths.subs(20, 10));
+	}
+	
+	@Test
+	public void testDiv() {
+		assertEquals(3, maths.div(9, 3));
+	}
+	
+	@Test
+	public void testZeroDiv() {
+		assertThrows(ArithmeticException.class, () -> maths.div(2,0));
+	}
+}
